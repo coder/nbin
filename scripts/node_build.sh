197de41192ab6eb922f8d7131d93a3134f2290e2
Kyle Carberry <kyle@kwc.io>
2019-03-21 15:40:20 -0400

    Update deploy configuration

--- scripts/node_build.sh
+++ scripts/node_build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+source ./vars.sh
 cd ../lib/node
 ./configure --link-module './nbin.js' --link-module './lib/_third_party_main.js'
-make -j8
+echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+make -j2
+echo -e "\ntravis_fold:end:$1\r"
+cd ../../
+
+mkdir -p ./build/$PACKAGE_VERSION
+OS=${OSTYPE%-gnu}
+ARCH=$(uname -p)
+BINARY_NAME="node-${NODE_VERSION}-${OS}-${ARCH}"
+
+cp ./lib/node/out/Release/node ./build/$PACKAGE_VERSION/$BINARY_NAME
---
+++ scripts/vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+NODE_VERSION=8.15.0
+PACKAGE_VERSION=$(cat ../package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
