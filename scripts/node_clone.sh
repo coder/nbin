1872f92b8bc7300e8a7778849c8abffeb31f1850
Kyle Carberry <kyle@kwc.io>
2019-03-21 14:50:46 -0400

    Update remote node version

--- scripts/node_clone.sh
+++ scripts/node_clone.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 cd ..
 mkdir -p lib
 cd lib
-git clone https://github.com/nodejs/node --branch 8.15.0 --depth 1
+git clone https://github.com/nodejs/node --branch v8.15.0 --depth 1
