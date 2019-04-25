f36c08f223e45d03b88eda29eaf535d071f1e50b
Kyle Carberry <kyle@kwc.io>
2019-04-25 19:11:23 -0400

    Statically set cores if on darwin

--- scripts/node_build.sh
+++ scripts/node_build.sh
@@ -7,7 +7,11 @@ export CC="ccache gcc"
 export CXX="ccache g++"
 export CCACHE_DIR="/ccache"
 ./configure --link-module './nbin.js' --link-module './lib/_third_party_main.js' --dest-cpu=x64 --openssl-no-asm
-cores=$(cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	cores="2"
+else
+	cores=$(cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l)
+fi
 make -j$cores &>/dev/null &
 pid=$!
 (
