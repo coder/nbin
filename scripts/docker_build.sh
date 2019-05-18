9ab1d5b020e98460e0f6b11eda5099851e9762ae
Kibo Hikari <enra@sayonika.moe>
2019-05-18 20:04:08 +0800

    refactor: armhf -> armv7hf
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -63,7 +63,7 @@ elif [[ "$TARGET" == "armv7hf-alpine" ]]; then
 	CACHE_DIR=".ccache-alpine"
 	IMAGE="codercom/nbin-alpine-armv7hf-alpine"
 	PREBUILD_COMMAND=""
-	BINARY_NAME="node-${NODE_VERSION}-alpine-armhf"
+	BINARY_NAME="node-${NODE_VERSION}-alpine-armv7hf"
 	docker_build
 elif [[ "$TARGET" == "aarch64" ]]; then
 	CACHE_DIR=".ccache-aarch64"
@@ -72,10 +72,10 @@ elif [[ "$TARGET" == "aarch64" ]]; then
 	BINARY_NAME="node-${NODE_VERSION}-aarch64"
 	docker_build
 elif [[ "$TARGET" == "armv7hf" ]]; then
-	CACHE_DIR=".ccache-armhf"
+	CACHE_DIR=".ccache-armv7hf"
 	IMAGE="codercom/nbin-armv7hf"
 	PREBUILD_COMMAND=""
-	BINARY_NAME="node-${NODE_VERSION}-armhf"
+	BINARY_NAME="node-${NODE_VERSION}-armv7hf"
 	docker_build
 else
 	CACHE_DIR=".ccache-centos"
