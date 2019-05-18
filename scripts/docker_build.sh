e6e7f4d652dd9004ce006700d5819a8b3c95b76a
Kibo Hikari <enra@sayonika.moe>
2019-05-18 19:40:17 +0800

    fix: fix build_docker not using correct images
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -34,25 +34,25 @@ if [[ "$TARGET" == "alpine" ]]; then
 	docker_build
 elif [[ "$TARGET" == "aarch64-alpine" ]]; then
 	CACHE_DIR=".ccache-alpine"
-	IMAGE="codercom/nbin-alpine"
+	IMAGE="codercom/nbin-alpine-aarch64-alpine"
 	PREBUILD_COMMAND=""
 	BINARY_NAME="node-${NODE_VERSION}-alpine-aarch64"
 	docker_build
 elif [[ "$TARGET" == "armv7hf-alpine" ]]; then
 	CACHE_DIR=".ccache-alpine"
-	IMAGE="codercom/nbin-alpine"
+	IMAGE="codercom/nbin-alpine-armv7hf-alpine"
 	PREBUILD_COMMAND=""
 	BINARY_NAME="node-${NODE_VERSION}-alpine-armhf"
 	docker_build
 elif [[ "$TARGET" == "aarch64" ]]; then
 	CACHE_DIR=".ccache-aarch64"
-	IMAGE="codercom/nbin-alpine"
+	IMAGE="codercom/nbin-aarch64"
 	PREBUILD_COMMAND=""
 	BINARY_NAME="node-${NODE_VERSION}-aarch64"
 	docker_build
 elif [[ "$TARGET" == "armv7hf" ]]; then
 	CACHE_DIR=".ccache-armhf"
-	IMAGE="codercom/nbin-alpine"
+	IMAGE="codercom/nbin-armv7hf"
 	PREBUILD_COMMAND=""
 	BINARY_NAME="node-${NODE_VERSION}-armhf"
 	docker_build
