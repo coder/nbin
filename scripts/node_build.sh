a323c96a06755a6672f903a1fa893b91506f50c0
Anmol Sethi <hi@nhooyr.io>
2019-06-07 14:09:49 -0400

    Use --openssl-use-def-ca-store when building node
    Closes #12

--- scripts/node_build.sh
+++ scripts/node_build.sh
@@ -12,7 +12,7 @@ else
 	export CCACHE_DIR="/ccache"
 	export cores=$(cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l)
 fi
-./configure --link-module './nbin.js' --link-module './lib/_third_party_main.js' --dest-cpu=x64 --openssl-no-asm
+./configure --link-module './nbin.js' --link-module './lib/_third_party_main.js' --dest-cpu=x64 --openssl-no-asm --openssl-use-def-ca-store
 make -j$cores &
 pid=$!
 (
