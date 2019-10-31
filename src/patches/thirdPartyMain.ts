b25ee25133c3eb2dc92200cd7641684eeb81d745
Asher <ash@coder.com>
2019-10-31 15:52:17 -0500

    Still use nbin when process.send exists
    In code-server we need to restart the process when upgrading but we also
    need IPC to communicate with the child and this made it so we can't do
    that without specifying a path to the main file which is not stable
    because the path can be different between binaries (at the very least
    the version will be different but there's no guarantee the rest of the
    path is the same either).
    
    This change makes it so you can control whether nbin is used for any
    spawn including with IPC, but it also means you need to be sure you set
    NBIN_BYPASS appropriately (code-server sets it immediately upon
    spawning to ensure nothing spawns/forks with nbin unless we explicitly
    pass `NBIN_BYPASS: undefined` to it).

--- src/patches/thirdPartyMain.ts
+++ src/patches/thirdPartyMain.ts
@@ -1,8 +1,8 @@
 /**
  * Application entrypoint.
- * 
+ *
  * Patched to execute then continue normal node instantiation.
- * 
+ *
  * We can set `process.argv` here to easily passthrough.
  */
 import * as nbin from "nbin";
@@ -17,8 +17,5 @@ process.versions.nbin = '<NBIN_VERSION>';
  * If bypassing nbin don't touch a thing.
  */
 if (!process.env.NBIN_BYPASS) {
-	if (!process.send) {
-		process.argv.splice(1, 0, nbin.mainFile);
-	}
+	process.argv.splice(1, 0, nbin.mainFile);
 }
-
